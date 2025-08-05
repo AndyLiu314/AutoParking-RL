2590671525033246438ea614b6706f2f753b20b0
Andy Liu <57054829+AndyLiu314@users.noreply.github.com>
2025-08-05 01:56:22 -0700

    Adjusted hyperparameters
    * Added extra layers to network for complexity
    * Emphasized slower movements/adjustments for agent learning
    * Increased training steps

--- PPO/ppo_train.py
+++ PPO/ppo_train.py
@@ -1,5 +1,6 @@
 import gymnasium as gym
 import highway_env
+import torch.nn
 
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
@@ -7,7 +8,7 @@ from stable_baselines3.common.vec_env import SubprocVecEnv
 
 if __name__ == "__main__":
     n_cpu = 4
-    batch_size = 64
+    batch_size = 256
 
     def custom_env():
         return gym.make("parking-v0", render_mode="rgb_array", config={
@@ -19,16 +20,23 @@ if __name__ == "__main__":
     model = PPO(
         "MultiInputPolicy",
         train_env,
-        policy_kwargs=dict(net_arch=[dict(pi=[256, 256], vf=[256, 256])]),
+        policy_kwargs=dict(
+            net_arch=dict(pi=[256, 256, 256], vf=[256, 256, 256]),
+            activation_fn=torch.nn.ReLU,
+            ortho_init=True
+        ),
+
+        clip_range=0.2,
         n_steps=batch_size * 12 // n_cpu,
         batch_size=batch_size,
         n_epochs=10,
-        learning_rate=5e-4,
-        gamma=0.8,
+        learning_rate=4e-4,
+        gamma=0.95,
+        ent_coef=0.01,
         verbose=2,
     )
 
     # Train the agent
-    model.learn(total_timesteps=int(2e4))
+    model.learn(total_timesteps=int(2e5))
     # Save the agent
     model.save("PPO_parking")
